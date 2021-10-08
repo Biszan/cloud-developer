6c6111b613910c2ed05c78ed7e75b6021e6f6071
BISZAN MUKWELLE <biszanelle@gmail.com>
2021-10-08 15:18:21 +0100

    Update server.ts

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -19,6 +19,51 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // IT SHOULD
   //    1
   //    1. validate the image_url query
+
+    // Assign route
+    app.use('/filteredimage', async (req, res, next) => {
+        //const filters = req.query;
+
+        // destruct our path params
+        let { image_url } = req.query;
+
+        console.log(image_url);
+        //console.log(filters);
+
+  //    1. validate the image_url query
+        if (!image_url) {
+            // respond with an error if not
+            return res.status(400).send(`image_url is required`);
+        }
+
+        //    2. call filterImageFromURL(image_url) to filter the image
+        // try to find the image path
+        const image_path = filterImageFromURL(image_url);
+
+        // respond not found, if we do not have this image path
+        if (image_path && (await image_path).length==0) {
+            return res.status(404).send(`image not found`);
+        }
+
+        //    3. send the resulting file in the response
+        //return the image with a sucess status code
+        res.status(200).sendFile(await image_path);
+
+  //    4. deletes any files on the server on finish of the response
+        // Delete the image from the filesystem
+        let fs = require('fs');
+
+        let fileNameWithPath = image_path;
+        console.log(fileNameWithPath);
+
+        if (fs.existsSync(fileNameWithPath)) {
+            fs.unlink(fileNameWithPath, (err: any) => {
+                console.log(err);
+            });
+        }
+
+    });
+
   //    2. call filterImageFromURL(image_url) to filter the image
   //    3. send the resulting file in the response
   //    4. deletes any files on the server on finish of the response
